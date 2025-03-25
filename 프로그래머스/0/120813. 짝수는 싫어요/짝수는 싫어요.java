09ab0daf1a144ea8ae8b40dd8ac7001d5e968535
gisu1102 <76506573+gisu1102@users.noreply.github.com>
2025-03-25 18:59:05 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -4,8 +4,12 @@ class Solution {
         // 1~n 이하 홀수 채워넣기
         int[] answer = new int[(n+1)/2];
         int index = 0;
-        for(int i=1; i<=n ; i+=2){
-            answer[index++] = i;
+        for(int i=1; i<=n ; i++){
+            if((i%2) != 0){
+                answer[index]=i;
+                index++;
+            }
+            
         }
         return answer;
     }
