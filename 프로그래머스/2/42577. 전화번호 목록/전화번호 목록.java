dc22e8dc130ed19d89c2bc8b6e19b8fc81a9b1fa
gisu1102 <76506573+gisu1102@users.noreply.github.com>
2025-10-07 20:40:30 +0900

    [level 2] Title: 전화번호 목록, Time: 297.70 ms, Memory: 254 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,17 +4,32 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         
-        Arrays.sort(phone_book);
+//         Arrays.sort(phone_book);
         
-        for (int i=0; i < phone_book.length -1; i++) {
-            String a = phone_book[i];
-            String b = phone_book[i+1];
+//         for (int i=0; i < phone_book.length -1; i++) {
+//             String a = phone_book[i];
+//             String b = phone_book[i+1];
             
-            if (b.length() >= a.length()){
-                if(a.equals(b.substring(0,a.length()))) answer=false;
-            }
-        }
+//             if (b.length() >= a.length()){
+//                 if(a.equals(b.substring(0,a.length()))) answer=false;
+//             }
+//         }
 
-        return answer;
+        HashSet<String> set = new HashSet<>();
+        
+        for (String s : phone_book) {
+            set.add(s);
+        }
+        
+        for (String num : phone_book) {
+            for(int i=0; i < num.length(); i++){
+                if (set.contains(num.substring(0, i))) {
+                    return false;
+                }
+            }
+
+        }
+        
+        return true;
     }
 }
