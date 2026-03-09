c4e1c274ac22d9ab7665127e21945dbc8289c664
gisu1102 <gisu1102@gmail.com>
2026-03-09 09:30:58 +0000

    Time: 11 ms (25.04%), Space: 20.3 MB (93.49%) - LeetHub

--- LeetCode/Easy/0268-missing-number/0268-missing-number.py
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.py
@@ -1,11 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
 
-        nums.sort()
-        cnt = 0
-        for i in range(len(nums)):
-            if nums[i] != cnt :
-                return(i)
-            cnt += 1
-        return(cnt)
+        # nums.sort()
+        # cnt = 0
+        # for i in range(len(nums)):
+        #     if nums[i] != cnt :
+        #         return(i)
+        #     cnt += 1
+        # return(cnt)
         
+        nums.sort()
+
+        for i,v in enumerate(nums):
+            if (i != v):
+                return v-1
+        return len(nums)
