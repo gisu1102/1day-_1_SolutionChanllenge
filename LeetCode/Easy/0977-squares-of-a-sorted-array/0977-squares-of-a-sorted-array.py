02142077c6c7430be5095e659eb8af33a60519e8
gisu1102 <gisu1102@gmail.com>
2026-03-10 10:43:36 +0000

    Time: 7 ms (79.73%), Space: 21.1 MB (68.74%) - LeetHub

--- LeetCode/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ LeetCode/Easy/0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -8,48 +8,63 @@ class Solution:
         
         # return sorted(res)
 
-        # 2. merge and sort
+        # # 2. merge and sort
 
-        #edge case
-        if not nums:
-            return nums
+        # #edge case
+        # if not nums:
+        #     return nums
         
-        if nums[0] >= 0:
-            return [num**2 for num in nums]
+        # if nums[0] >= 0:
+        #     return [num**2 for num in nums]
 
-        #find index first positive = 0
-        m = len(nums)
-        for i, v in enumerate(nums):
-            if v >= 0:
-                m = i
-                break
+        # #find index first positive = 0
+        # m = len(nums)
+        # for i, v in enumerate(nums):
+        #     if v >= 0:
+        #         m = i
+        #         break
 
-        #A p nums
-        A = []
-        for p_num in nums[m:]:
-            A.append(p_num**2)
-        #B n nums
-        B = []
-        for n_num in reversed(nums[:m]):
-            B.append(n_num**2)
+        # #A p nums
+        # A = []
+        # for p_num in nums[m:]:
+        #     A.append(p_num**2)
+        # #B n nums
+        # B = []
+        # for n_num in reversed(nums[:m]):
+        #     B.append(n_num**2)
 
-        def merge(A,B):
-            a,b = 0,0
-            ans = []
-            while a < len(A) and b <len(B):
-                #비교후 추가
-                if A[a] < B[b]:
-                    ans.append(A[a])
-                    a+=1
-                else :
-                    ans.append(B[b])
-                    b+=1
-            #둘 중 하나가 끝까지 갔을 때
-            if a<len(A):
-                ans.extend(A[a:])
-            else:
-                ans.extend(B[b:])
+        # def merge(A,B):
+        #     a,b = 0,0
+        #     ans = []
+        #     while a < len(A) and b <len(B):
+        #         #비교후 추가
+        #         if A[a] < B[b]:
+        #             ans.append(A[a])
+        #             a+=1
+        #         else :
+        #             ans.append(B[b])
+        #             b+=1
+        #     #둘 중 하나가 끝까지 갔을 때
+        #     if a<len(A):
+        #         ans.extend(A[a:])
+        #     else:
+        #         ans.extend(B[b:])
             
-            return ans
+        #     return ans
         
-        return(merge(A,B))
+        # return(merge(A,B))
+
+        # 3. two pointers
+        ans = collections.deque()
+        l,r = 0, len(nums) - 1
+        
+        while l<=r:
+            left, right = abs(nums[l]), abs(nums[r])
+
+            if left > right:
+                ans.appendleft(left**2)
+                l+=1
+            else:
+                ans.appendleft(right**2)
+                r-=1
+        return list(ans)
