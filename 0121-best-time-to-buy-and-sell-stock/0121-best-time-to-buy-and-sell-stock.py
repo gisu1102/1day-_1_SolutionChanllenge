a38f696510819c1c72c7f6aee60224a00c54f38d
gisu1102 <76506573+gisu1102@users.noreply.github.com>
2026-03-10 18:57:55 +0900

    Time: 76 ms (11.46%), Space: 28.6 MB (93.11%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        left = 0 # Left
+        right = 1 # Right
+        max_profit = 0
+
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] -prices[left]
+                max_profit = max(profit, max_profit)
+                right += 1
+            else :
+                left = right
+                right += 1
+        return max_profit
+
+
+        
